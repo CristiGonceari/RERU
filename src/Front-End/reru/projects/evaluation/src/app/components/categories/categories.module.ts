3e6d512a189690220691cca304e3bb9aef0f877c
DESKTOP-DTIIHBO\Tanya <tatiana.agachi@codwer.com>
2021-11-05 12:47:39 +0200

    categories FE

--- src/Front-End/reru/projects/evaluation/src/app/components/categories/categories.module.ts
+++ src/Front-End/reru/projects/evaluation/src/app/components/categories/categories.module.ts
@@ -4,10 +4,17 @@ import { CommonModule } from '@angular/common';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoriesComponent } from './categories.component';
 import { AddEditCategoryComponent } from './add-edit-category/add-edit-category.component';
+import { CategoryDetailsComponent } from './category-details/category-details.component';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryOverviewComponent } from './category-details/category-overview/category-overview.component';
+import { CategoryQuestionsComponent } from './category-details/category-questions/category-questions.component';
+import { SearchCategoryComponent } from './category-list/search-category/search-category.component';
+import { CategoryNameComponent } from './category-list/category-name/category-name.component';
+import { CategoryListTableComponent } from './category-list/category-list-table/category-list-table.component';
 
 
 @NgModule({
-  declarations: [CategoriesComponent, AddEditCategoryComponent],
+  declarations: [CategoriesComponent, AddEditCategoryComponent, CategoryDetailsComponent, CategoryListComponent, CategoryOverviewComponent, CategoryQuestionsComponent, SearchCategoryComponent, CategoryNameComponent, CategoryListTableComponent],
   imports: [
     CommonModule,
     CategoriesRoutingModule
