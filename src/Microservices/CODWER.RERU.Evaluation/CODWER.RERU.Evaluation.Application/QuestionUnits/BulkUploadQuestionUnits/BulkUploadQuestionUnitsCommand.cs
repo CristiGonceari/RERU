4f21860eea29bfdd6baae44c0c184b2f9e893956
Pogor Cleopatra <cleopatra.pogor@codwer.com>
2021-11-08 12:41:22 +0200

    import questions from excel

---
+++ src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/QuestionUnits/BulkUploadQuestionUnits/BulkUploadQuestionUnitsCommand.cs
@@ -0,0 +1,13 @@
+﻿using CODWER.RERU.Evaluation.Application.Permissions;
+using CVU.ERP.Module.Application.Attributes;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace CODWER.RERU.Evaluation.Application.QuestionUnits.BulkUploadQuestionUnits
+{
+    [ModuleOperation(permission: PermissionCodes.QUESTION_UNITS_GENERAL_ACCESS)]
+    public class BulkUploadQuestionUnitsCommand : IRequest<byte[]>
+    {
+        public IFormFile Input { get; set; }
+    }
+}
---
+++ src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/QuestionUnits/BulkUploadQuestionUnits/BulkUploadQuestionUnitsCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using CODWER.RERU.Evaluation.Application.Services;
+using MediatR;
+
+namespace CODWER.RERU.Evaluation.Application.QuestionUnits.BulkUploadQuestionUnits
+{
+    public class BulkUploadQuestionUnitsCommandHandler : IRequestHandler<BulkUploadQuestionUnitsCommand, byte[]>
+    {
+        private readonly IQuestionUnitService _questionUnitService;
+
+        public BulkUploadQuestionUnitsCommandHandler(IQuestionUnitService questionUnitService)
+        {
+            _questionUnitService = questionUnitService;
+        }
+
+        public async Task<byte[]> Handle(BulkUploadQuestionUnitsCommand request, CancellationToken cancellationToken)
+        {
+            return await _questionUnitService.BulkQuestionsUpload(request.Input);
+        }
+    }
+}
