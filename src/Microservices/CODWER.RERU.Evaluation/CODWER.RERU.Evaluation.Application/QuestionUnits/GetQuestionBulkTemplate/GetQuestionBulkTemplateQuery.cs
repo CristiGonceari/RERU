80d582ad012a76425507a6defac088911783906b
Pogor Cleopatra <cleopatra.pogor@codwer.com>
2021-11-08 11:16:18 +0200

    fix json

---
+++ src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/QuestionUnits/GetQuestionBulkTemplate/GetQuestionBulkTemplateQuery.cs
@@ -0,0 +1,14 @@
+﻿using CODWER.RERU.Evaluation.Application.Permissions;
+using CODWER.RERU.Evaluation.Data.Entities.Enums;
+using CVU.ERP.Module.Application.Attributes;
+using MediatR;
+
+namespace CODWER.RERU.Evaluation.Application.QuestionUnits.GetQuestionBulkTemplate
+{
+    [ModuleOperation(permission: PermissionCodes.QUESTION_UNITS_GENERAL_ACCESS)]
+
+    public class GetQuestionBulkTemplateQuery : IRequest<byte[]>
+    {
+        public QuestionTypeEnum QuestionType { get; set; }
+    }
+}
---
+++ src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/QuestionUnits/GetQuestionBulkTemplate/GetQuestionBulkTemplateQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using CODWER.RERU.Evaluation.Application.Services;
+using MediatR;
+
+namespace CODWER.RERU.Evaluation.Application.QuestionUnits.GetQuestionBulkTemplate
+{
+    public class GetQuestionBulkTemplateQueryHandler : IRequestHandler<GetQuestionBulkTemplateQuery, byte[]>
+    {
+        private readonly IQuestionUnitService _questionUnitService;
+
+        public GetQuestionBulkTemplateQueryHandler(IQuestionUnitService questionUnitService)
+        {
+            _questionUnitService = questionUnitService;
+        }
+
+        public async Task<byte[]> Handle(GetQuestionBulkTemplateQuery request, CancellationToken cancellationToken)
+        {
+            return await _questionUnitService.GenerateExcelTemplate(request.QuestionType);
+        }
+    }
+}
