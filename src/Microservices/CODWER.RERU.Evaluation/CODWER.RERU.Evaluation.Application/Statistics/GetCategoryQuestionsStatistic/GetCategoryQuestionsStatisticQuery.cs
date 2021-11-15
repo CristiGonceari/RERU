fde787b6b947e5cdc01542bf3e5580726d965733
Pogor Cleopatra <cleopatra.pogor@codwer.com>
2021-11-15 15:53:55 +0200

    add statistics functional on be

---
+++ src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/Statistics/GetCategoryQuestionsStatistic/GetCategoryQuestionsStatisticQuery.cs
@@ -0,0 +1,17 @@
+﻿using CVU.ERP.Module.Application.Attributes;
+using MediatR;
+using System.Collections.Generic;
+using CODWER.RERU.Evaluation.Application.Permissions;
+using CODWER.RERU.Evaluation.Data.Entities.Enums;
+using CODWER.RERU.Evaluation.DataTransferObjects.Statistics;
+
+namespace CODWER.RERU.Evaluation.Application.Statistics.GetCategoryQuestionsStatistic
+{
+    [ModuleOperation(permission: PermissionCodes.STATISTICS_GENERAL_ACCESS)]
+    public class GetCategoryQuestionsStatisticQuery : IRequest<List<TestQuestionStatisticDto>>
+    {
+        public int CategoryId { get; set; }
+        public int ItemsPerPage { get; set; }
+        public StatisticsQuestionFilterEnum FilterEnum { get; set; }
+    }
+}
---
+++ src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/Statistics/GetTestQuestionsStatistic/GetTestQuestionsStatisticQueryHandler.cs
@@ -0,0 +1,147 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CODWER.RERU.Evaluation.Data.Entities.Enums;
+using CODWER.RERU.Evaluation.Data.Persistence.Context;
+using CODWER.RERU.Evaluation.DataTransferObjects.Statistics;
+
+namespace CODWER.RERU.Evaluation.Application.Statistics.GetTestQuestionsStatistic
+{
+    public class GetTestQuestionsStatisticQueryHandler : IRequestHandler<GetTestQuestionsStatisticQuery, List<TestQuestionStatisticDto>>
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly IMapper _mapper;
+
+        public GetTestQuestionsStatisticQueryHandler(AppDbContext appDbContext, IMapper mapper)
+        {
+            _appDbContext = appDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<List<TestQuestionStatisticDto>> Handle(GetTestQuestionsStatisticQuery request, CancellationToken cancellationToken)
+        {
+            var testTypeQuestionCategories = await _appDbContext.TestTypeQuestionCategories.Where(x => x.TestTypeId == request.TestTypeId).ToListAsync();
+
+            var testQuestionsId = new List<int>();
+
+            foreach (var testTypeQuestionCategory in testTypeQuestionCategories)
+            {
+                var allQuestions = _appDbContext.QuestionUnits
+                    .Where(x => x.QuestionCategoryId == testTypeQuestionCategory.QuestionCategoryId && x.Status == QuestionUnitStatusEnum.Active)
+                    .AsQueryable();
+
+                if (testTypeQuestionCategory.QuestionType.HasValue)
+                {
+                    allQuestions = allQuestions.Where(x => x.QuestionType == testTypeQuestionCategory.QuestionType.Value);
+                }
+
+                var questionIds = allQuestions.Select(x => x.Id);
+                var strictQuestionsToUse = _appDbContext.TestCategoryQuestions.Include(x => x.QuestionUnit).Where(x => x.TestTypeQuestionCategoryId == testTypeQuestionCategory.Id).AsQueryable();
+
+                if (testTypeQuestionCategory.SelectionType == SelectionEnum.Select)
+                {
+                    questionIds = strictQuestionsToUse.Select(x => x.QuestionUnitId);
+                }
+
+                testQuestionsId.AddRange(await questionIds.ToListAsync());
+
+            }
+
+            var questionsInTests = await _appDbContext.Tests
+                .Include(x => x.TestQuestions)
+                .Where(x => x.TestTypeId == request.TestTypeId)
+                .SelectMany(x => x.TestQuestions)
+                .ToListAsync();
+
+            var questionInTestType = await _appDbContext.QuestionUnits.Include(x => x.QuestionCategory).Where(x => testQuestionsId.Contains(x.Id)).ToListAsync();
+
+            var answer = new List<TestQuestionStatisticDto>();
+
+            foreach (var questionToAnalize in questionInTestType)
+            {
+                if (!questionsInTests.Any(x => x.QuestionUnitId == questionToAnalize.Id))
+                {
+                    answer.Add(new TestQuestionStatisticDto()
+                    {
+                        QuestionId = questionToAnalize.Id,
+                        TotalUsed = 0,
+                        CountByFilter = 0,
+                        Percent = 0,
+                        CountCorrect = 0,
+                        PercentCorrect = 0,
+                        CountNotCorrect = 0,
+                        PercentNotCorrect = 0,
+                        CountSkipped = 0,
+                        PercentSkipped = 0,
+                        Question = questionToAnalize.Question,
+                        CategoryName = questionToAnalize.QuestionCategory.Name
+                    });
+                }
+                else
+                {
+                    var questionsArray = questionsInTests.Where(x => x.QuestionUnitId == questionToAnalize.Id).ToList();
+                    var total = questionsArray.Count();
+                    var correct = questionsArray.Where(x => x.IsCorrect == true).Count();
+                    var notCorrect = questionsArray.Where(x => x.IsCorrect == false && x.AnswerStatus == AnswerStatusEnum.Answered).Count();
+                    var skipped = questionsArray.Where(x => x.AnswerStatus == AnswerStatusEnum.Skipped).Count();
+
+                    answer.Add(new TestQuestionStatisticDto()
+                    {
+                        QuestionId = questionToAnalize.Id,
+                        TotalUsed = total,
+                        CountCorrect = correct,
+                        PercentCorrect = 100 * correct / total,
+                        CountNotCorrect = notCorrect,
+                        PercentNotCorrect = 100 * notCorrect / total,
+                        CountSkipped = skipped,
+                        PercentSkipped = 100 * skipped / total,
+                        Question = questionToAnalize.Question,
+                        CategoryName = questionToAnalize.QuestionCategory.Name
+                    });
+                }
+            }
+
+            if (request.FilterEnum == StatisticsQuestionFilterEnum.Correct)
+            {
+                foreach (var ans in answer)
+                {
+                    if (ans.TotalUsed != 0)
+                    {
+                        ans.CountByFilter = ans.CountCorrect;
+                        ans.Percent = ans.PercentCorrect;
+                    }
+                }
+            }
+
+            if (request.FilterEnum == StatisticsQuestionFilterEnum.NotCorrect)
+            {
+                foreach (var ans in answer)
+                {
+                    if (ans.TotalUsed != 0)
+                    {
+                        ans.CountByFilter = ans.CountNotCorrect;
+                        ans.Percent = ans.PercentNotCorrect;
+                    }
+                }
+            }
+
+            if (request.FilterEnum == StatisticsQuestionFilterEnum.Skipped)
+            {
+                foreach (var ans in answer)
+                {
+                    if (ans.TotalUsed != 0)
+                    {
+                        ans.CountByFilter = ans.CountSkipped;
+                        ans.Percent = ans.PercentSkipped;
+                    }
+                }
+            }
+
+            return answer.OrderByDescending(x => x.CountByFilter).Take(request.ItemsPerPage).ToList();
+        }
+    }
+}
