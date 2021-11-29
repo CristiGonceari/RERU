b459fa53a9a9e2ac8e6f633b13ceead15dc306f0
Pogor Cleopatra <cleopatra.pogor@codwer.com>
2021-11-29 14:17:10 +0200

    fix permiisions for user

--- src/Front-End/reru/projects/evaluation/src/app/components/tests/tests-routing.module.ts
+++ src/Front-End/reru/projects/evaluation/src/app/components/tests/tests-routing.module.ts
@@ -3,23 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { TestListComponent } from '../tests/test-list/test-list.component';
 import { AddTestComponent } from './add-test/add-test.component';
 import { TestVerificationProcessComponent } from './test-verification-process/test-verification-process.component';
-import { FinishPageComponent } from './finish-page/finish-page.component';
-import { OnePerPagePerformingTestComponent } from './one-per-page-performing-test/one-per-page-performing-test.component';
-import { StartTestPageComponent } from './start-test-page/start-test-page.component';
-import { MultiplePerPagePerformingTestComponent } from './multiple-per-page-performing-test/multiple-per-page-performing-test.component';
-import { ViewTestResultComponent } from './view-test-result/view-test-result.component';
-import { TestListTableComponent } from './test-list-table/test-list-table.component';
 
 const routes: Routes = [
   { path: '', component: TestListComponent },
-  { path: 'one-test-per-page/:id', component: OnePerPagePerformingTestComponent },
-  { path: 'multiple-per-page/:id', component: MultiplePerPagePerformingTestComponent},
-  { path: 'start-test/:id', component: StartTestPageComponent },
   { path: 'add-test', component: AddTestComponent },
-	{ path: 'finish-page/:id', component: FinishPageComponent },
-  { path: 'start-test/:id', component: StartTestPageComponent },
   { path: 'verify-test/:id', component: TestVerificationProcessComponent },
-  { path: 'test-result/:id', component: ViewTestResultComponent },
 ];
 
 @NgModule({
