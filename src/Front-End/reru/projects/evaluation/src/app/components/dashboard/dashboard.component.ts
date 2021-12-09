d20369455fa42002834c9416dd33bfea0810f8be
CristianGonceari <cristian.gonceari@codwer.com>
2021-12-09 17:19:25 +0200

    Cards for Upload and Dowloand Files from Cloud

--- src/Front-End/reru/projects/evaluation/src/app/components/dashboard/dashboard.component.ts
+++ src/Front-End/reru/projects/evaluation/src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserProfileService } from './../../utils/services/user-profile/user-profile.service';
+import { saveAs } from 'file-saver';
+import { CloudFileService } from '../../utils/services/cloud-file/cloud-file.service';
+import { NotificationUtil } from '../../utils/util/notification.util';
+import { NotificationsService } from 'angular2-notifications';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,7 +13,17 @@ import { UserProfileService } from './../../utils/services/user-profile/user-pro
 export class DashboardComponent implements OnInit {
   profile;
   isLoading: boolean = true;
-  constructor(private userService: UserProfileService) { }
+  fileId;
+  seIncarca: boolean = true;
+  seIncarca1: boolean = true;
+  fileType;
+  files;
+  lastId;
+
+  constructor(private userService: UserProfileService,
+              private fileService : CloudFileService,
+              private notificationService: NotificationsService,
+    ) { }
 
   ngOnInit(): void {
     this.retrieveProfile();
@@ -21,4 +35,36 @@ export class DashboardComponent implements OnInit {
       this.isLoading = false;
     })
   }
+
+  downloadFile(item): void {
+    this.seIncarca = false;
+    this.fileService.get(item).subscribe(response => {
+      const fileName = response.name;
+      const blob = new Blob([response.body], { type: response.body.type });
+			const file = new File([blob], fileName, { type: response.body.type });
+      saveAs(file);
+      this.seIncarca = true;
+    });
+  }
+  uploadFile(): void
+  {
+    this.seIncarca1 = false;
+    const request = new FormData();
+    request.append('File', this.files);
+    request.append('Type', this.fileType);
+    this.fileService.create(request).subscribe(res => {
+      this.lastId = res.data;
+      this.notificationService.success('Success', 'Fișier adăugat!', NotificationUtil.getDefaultConfig());
+      this.seIncarca1 = true;
+    }, error =>
+    {
+      this.notificationService.warn('Error', NotificationUtil.getDefaultConfig());
+      this.seIncarca1 = true;
+    })
+  }
+  
+  onFileChange(event){
+    this.files = event.target.files[0];
+  }
+  
 }
