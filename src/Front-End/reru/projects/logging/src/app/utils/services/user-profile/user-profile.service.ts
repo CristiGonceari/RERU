6ce0bc442fd3adc9e5f2fa9fa6dbc55d34b5b2b0
gordelenco <cristian.gordelenco@codwer.com>
2021-12-27 18:07:19 +0200

    Delete Functions From user-profile service

--- src/Front-End/reru/projects/logging/src/app/utils/services/user-profile/user-profile.service.ts
+++ src/Front-End/reru/projects/logging/src/app/utils/services/user-profile/user-profile.service.ts
@@ -25,23 +25,4 @@ export class UserProfileService extends AbstractService {
     return this.http.get(`${this.baseUrl}/${this.urlRoute}/list`, { params });
   }
 
-  getUserProfilesByLocation(params): Observable<any> {
-    return this.http.get(`${this.baseUrl}/LocationResponsiblePerson/no-assigned`, { params });
-  }
-
-  getUserProfilesByEvent(params): Observable<any> {
-    return this.http.get(`${this.baseUrl}/EventResponsiblePerson/no-assigned`, { params });
-  }
-
-  getUserProfilesByPlan(params): Observable<any> {
-    return this.http.get(`${this.baseUrl}/${this.urlRoute}/not-attached-plan`, { params });
-  }
-
-  getUserProfilesByAttachedUserEvent(params): Observable<any> {
-    return this.http.get(`${this.baseUrl}/EventUser/no-assigned`, { params });
-  }
-
-  getUserProfilesByEvaluatorEvent(params): Observable<any> {
-    return this.http.get(`${this.baseUrl}/EventEvaluator/no-assigned`, { params });
-  }
 }
