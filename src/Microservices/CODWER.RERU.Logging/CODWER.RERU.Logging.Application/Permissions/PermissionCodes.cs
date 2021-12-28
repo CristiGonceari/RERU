fa41960306a23d850ceb43bf6ff27bd3676b3d79
ANDRIAN-HOME-WORKSTATION <andrian.hubencu@codwer.com>
2021-12-28 12:39:37 +0200

    add permission fucntional to logging

---
+++ src/Microservices/CODWER.RERU.Logging/CODWER.RERU.Logging.Application/Permissions/PermissionCodes.cs
@@ -0,0 +1,7 @@
+﻿namespace CODWER.RERU.Logging.Application.Permissions
+{
+    public static class PermissionCodes
+    {
+        public const string LOGGING_GENERAL_ACCESS = "P04000001";
+    }
+}
