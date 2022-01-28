51eaa51719c60dc6005d3562bbf08011da3d940f
CristianGonceari <cristian.gonceari@codwer.com>
2022-01-28 12:43:22 +0200

    Fix Personal BE

---
+++ src/Microservices/CODWER.RERU.Personal/CODWER.RERU.Personal.Data.Persistence/Migrations/20220127134229_Add_FileSignature.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CODWER.RERU.Personal.Data.Persistence.Migrations
+{
+    public partial class Add_FileSignature : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "UserProfiles",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "FileSignatures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContractorId = table.Column<int>(type: "int", nullable: false),
+                    FileId = table.Column<int>(type: "int", nullable: false),
+                    CreateById = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdateById = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeleteTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileSignatures", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FileSignatures_ByteFiles_FileId",
+                        column: x => x.FileId,
+                        principalTable: "ByteFiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_FileSignatures_Contractors_ContractorId",
+                        column: x => x.ContractorId,
+                        principalTable: "Contractors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FileSignatures_ContractorId",
+                table: "FileSignatures",
+                column: "ContractorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FileSignatures_FileId",
+                table: "FileSignatures",
+                column: "FileId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FileSignatures");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "UserProfiles");
+        }
+    }
+}
