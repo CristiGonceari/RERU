8b858de492e2f535584e665809e980d166dc298f
andrian.hubencu <andrian.hubencu@codwer.com>
2022-01-28 17:57:02 +0200

    copy current header to EvaluationClient request

--- src/Common/CODWER.Common/CVU.ERP.Module.Application/src/Clients/EvaluationClient.cs
+++ src/Common/CODWER.Common/CVU.ERP.Module.Application/src/Clients/EvaluationClient.cs
@@ -7,6 +7,7 @@ using CVU.ERP.Module.Application.Exceptions;
 using CVU.ERP.Module.Application.Models.Internal;
 using CVU.ERP.Module.Common.Models;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
 using RestSharp;
 
@@ -16,11 +17,15 @@ namespace CVU.ERP.Module.Application.Clients
     {
         private readonly IRestClient _restClient;
         const string UserProfileBasePath = "/internaluserprofile";
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public EvaluationClient(IRestClient restClient, IOptions<ModuleConfiguration> moduleConfiguration)
+        public EvaluationClient(IRestClient restClient,
+            IOptions<ModuleConfiguration> moduleConfiguration,
+            IHttpContextAccessor httpContextAccessor)
         {
             _restClient = restClient;
             _restClient.BaseUrl = new Uri(moduleConfiguration.Value.EvaluationClient.BaseUrl);
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task SyncUserProfile(BaseUserProfile userProfile)
@@ -28,6 +33,11 @@ namespace CVU.ERP.Module.Application.Clients
             var request = new RestRequest(UserProfileBasePath, DataFormat.Json);
             var json = JsonSerializer.Serialize(userProfile);
             request.AddParameter("application/json; charset=utf-8", json, ParameterType.RequestBody);
+
+            foreach (var el in _httpContextAccessor?.HttpContext?.Request.Headers)
+            {
+                request.AddHeader(el.Key, el.Value);
+            }
 
             var response = await _restClient.PostAsync<Response<Unit>>(request, new CancellationToken());
 
