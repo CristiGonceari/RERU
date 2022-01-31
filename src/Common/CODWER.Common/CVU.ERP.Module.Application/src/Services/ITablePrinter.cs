177030c54607231ca78b4581e9448ace42020338
andrian.hubencu <andrian.hubencu@codwer.com>
2022-01-31 19:31:22 +0200

    generic service for printing table

---
+++ src/Common/CODWER.Common/CVU.ERP.Module.Application/src/Services/ITablePrinter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CVU.ERP.Module.Application.Services
+{
+    public interface ITablePrinter <T>
+    {
+        public string FormatTable(List<T> items, List<string> fields);
+    }
+}
