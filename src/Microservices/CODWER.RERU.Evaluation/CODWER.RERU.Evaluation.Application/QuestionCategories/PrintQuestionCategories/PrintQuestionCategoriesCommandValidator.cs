25e29c5363e36b0893f67ed9bd3b049b2d33e7f4
Tatiana <tatiana.ghijitchi@codwer.com>
2022-02-09 14:59:43 +0200

    Fix bulk import, add validations for print test, question, categories, plans, test templates and statistic

---
+++ src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/QuestionCategories/PrintQuestionCategories/PrintQuestionCategoriesCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using CODWER.RERU.Evaluation.Application.Validation;
+using CODWER.RERU.Evaluation.DataTransferObjects.QuestionCategory;
+using CVU.ERP.Common.Validation;
+using CVU.ERP.Module.Application.TablePrinterService.Validators;
+using FluentValidation;
+using System.Linq;
+
+namespace CODWER.RERU.Evaluation.Application.QuestionCategories.PrintQuestionCategories
+{
+    public class PrintQuestionCategoriesCommandValidator : AbstractValidator<PrintQuestionCategoriesCommand>
+    {
+        public PrintQuestionCategoriesCommandValidator()
+        {
+            RuleFor(x => x.Fields.Select(x => x.Value).ToList())
+                .SetValidator(new TablePrinterValidator<QuestionCategoryDto>(ValidationMessages.InvalidInput, ValidationCodes.INVALID_INPUT));
+        }
+    }
+}
---
+++ src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/Tests/PrintTests/PrintTestsCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using CODWER.RERU.Evaluation.Application.Validation;
+using CODWER.RERU.Evaluation.DataTransferObjects.Tests;
+using CVU.ERP.Common.Validation;
+using CVU.ERP.Module.Application.TablePrinterService.Validators;
+using FluentValidation;
+using System.Linq;
+
+namespace CODWER.RERU.Evaluation.Application.Tests.PrintTests
+{
+    public class PrintTestsCommandValidator : AbstractValidator<PrintTestsCommand>
+    {
+        public PrintTestsCommandValidator()
+        {
+            RuleFor(x => x.Fields.Select(x => x.Value).ToList())
+                .SetValidator(new TablePrinterValidator<TestDto>(ValidationMessages.InvalidInput, ValidationCodes.INVALID_INPUT));
+        }
+    }
+}
