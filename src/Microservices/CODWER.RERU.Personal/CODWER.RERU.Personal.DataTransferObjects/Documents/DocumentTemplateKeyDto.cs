6ed676930b8c1ea0468b4d1f5f66aed813613350
cristian.gonceari@codwer.com <cristian.gonceari@codwer.com>
2022-03-22 13:29:04 +0200

    Service for replace keys in documentsTemplates by contractorID

---
+++ src/Microservices/CODWER.RERU.Personal/CODWER.RERU.Personal.DataTransferObjects/Documents/DocumentTemplateKeyDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace CODWER.RERU.Personal.DataTransferObjects.Documents
+{
+    public class DocumentTemplateKeyDto
+    {
+        public int Id { get; set; }
+        public string KeyName { get; set; }
+        public string Description { get; set; }
+
+        public int DocumentCategoriesId { get; set; }
+    }
+}
