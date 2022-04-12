e9ba83e80f7989afc9b115a4d0af70c38d143d92
tatiana.agachi@codwer.com <tatiana.agachi@codwer.com>
2022-04-12 18:42:23 +0300

    format file

--- src/Front-End/reru/projects/core/src/app/components/positions/add-edit-position/add-edit-position.component.ts
+++ src/Front-End/reru/projects/core/src/app/components/positions/add-edit-position/add-edit-position.component.ts
@@ -10,57 +10,57 @@ import { NotificationUtil } from '../../../utils/util/notification.util';
 import { CandidatePositionModel } from '../../../utils/models/candidate-position.model';
 
 @Component({
-  selector: 'app-add-edit-position',
-  templateUrl: './add-edit-position.component.html',
-  styleUrls: ['./add-edit-position.component.scss']
+	selector: 'app-add-edit-position',
+	templateUrl: './add-edit-position.component.html',
+	styleUrls: ['./add-edit-position.component.scss']
 })
 export class AddEditPositionComponent implements OnInit {
-  isLoading: boolean = true;
-  positionId: number;
+	isLoading: boolean = true;
+	positionId: number;
 	positionForm: FormGroup;
-  positionName: string;
+	positionName: string;
 	title: string;
 	description: string;
 
-  constructor(
+	constructor(
 		private fb: FormBuilder,
 		private notificationService: NotificationsService,
 		public translate: I18nService,
 		private route: ActivatedRoute,
-    private positionService: CandidatePositionService,
+		private positionService: CandidatePositionService,
 		private location: Location
-  ) { }
+	) { }
 
-  ngOnInit(): void {
-    this.positionForm = new FormGroup({
-      name: new FormControl(),
-      isActive: new FormControl()
-    });
-    this.route.params.subscribe(params => {
+	ngOnInit(): void {
+		this.positionForm = new FormGroup({
+			name: new FormControl(),
+			isActive: new FormControl()
+		});
+		this.route.params.subscribe(params => {
 			if (params.id) {
 				this.positionId = params.id;
-        this.positionService.get(this.positionId).subscribe( res => {
-          this.initForm(res.data);
-        })
-        this.isLoading = false;
+				this.positionService.get(this.positionId).subscribe(res => {
+					this.initForm(res.data);
+				})
+				this.isLoading = false;
 			} else {
-        this.initForm();
-        this.isLoading = false;
-      }
+				this.initForm();
+				this.isLoading = false;
+			}
 		});
-  }
+	}
 
-  initForm(data?): void {
+	initForm(data?): void {
 		this.positionForm = this.fb.group({
 			name: this.fb.control(data?.name || null, [Validators.required]),
 			isActive: this.fb.control(data?.isActive || false)
 		});
 	}
 
-  addRole(): void {
+	addRole(): void {
 		let addPositionModel = {
 			name: this.positionForm.value.name,
-			isActive : this.positionForm.value.isActive,
+			isActive: this.positionForm.value.isActive,
 		} as CandidatePositionModel;
 		this.positionService.create(addPositionModel).subscribe(
 			() => {
@@ -70,7 +70,7 @@ export class AddEditPositionComponent implements OnInit {
 				]).subscribe(([title, description]) => {
 					this.title = title;
 					this.description = description;
-					});
+				});
 				this.notificationService.success(this.title, this.description, NotificationUtil.getDefaultMidConfig());
 				this.back();
 			}
@@ -81,7 +81,7 @@ export class AddEditPositionComponent implements OnInit {
 		let editPositionModel = {
 			id: +this.positionId,
 			name: this.positionForm.value.name,
-			isActive : this.positionForm.value.isActive
+			isActive: this.positionForm.value.isActive
 		} as CandidatePositionModel;
 		this.positionService.editPosition(editPositionModel).subscribe(
 			() => {
@@ -91,7 +91,7 @@ export class AddEditPositionComponent implements OnInit {
 				]).subscribe(([title, description]) => {
 					this.title = title;
 					this.description = description;
-					});
+				});
 				this.notificationService.success(this.title, this.description, NotificationUtil.getDefaultMidConfig());
 				this.back();
 			}
