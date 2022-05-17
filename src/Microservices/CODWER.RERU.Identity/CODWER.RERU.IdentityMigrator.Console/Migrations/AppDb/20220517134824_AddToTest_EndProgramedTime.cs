1396be76417c3ba176da9d76fc5fd2c56bb4c553
cristian.gonceari@codwer.com <cristian.gonceari@codwer.com>
2022-05-17 18:10:57 +0300

    Make Test with Event valid for oll event time

---
+++ src/Microservices/CODWER.RERU.Identity/CODWER.RERU.IdentityMigrator.Console/Migrations/AppDb/20220517134824_AddToTest_EndProgramedTime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CODWER.RERU.IdentityMigrator.Console.Migrations.AppDb
+{
+    public partial class AddToTest_EndProgramedTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndProgrammedTime",
+                table: "Tests",
+                type: "timestamp without time zone",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndProgrammedTime",
+                table: "Tests");
+        }
+    }
+}
