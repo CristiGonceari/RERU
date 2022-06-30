e472501017dd190eeb7daa129f8dabb54ed47483
Pogor Cleopatra <cleopatra.pogor@codwer.com>
2022-06-30 13:24:05 +0300

    change view poll record in tests table

--- src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/TestTemplates/GetTestTemplate/GetTestTemplateQuery.cs
+++ src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/TestTemplates/GetTestTemplate/GetTestTemplateQuery.cs
@@ -1,12 +1,8 @@
-﻿using CODWER.RERU.Evaluation.Application.Permissions;
-using CODWER.RERU.Evaluation.DataTransferObjects.TestTemplates;
-using CVU.ERP.Module.Application.Attributes;
+﻿using CODWER.RERU.Evaluation.DataTransferObjects.TestTemplates;
 using MediatR;
 
 namespace CODWER.RERU.Evaluation.Application.TestTemplates.GetTestTemplate
 {
-    [ModuleOperation(permission: PermissionCodes.ACCES_GENERAL_LA_SABLOANELE_DE_TESTE)]
-
     public class GetTestTemplateQuery : IRequest<TestTemplateDto>
     {
         public int Id { get; set; }
