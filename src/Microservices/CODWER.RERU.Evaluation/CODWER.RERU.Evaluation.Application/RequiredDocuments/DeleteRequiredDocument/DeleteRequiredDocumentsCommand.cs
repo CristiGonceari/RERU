73a0b0cfd11173cd9e461b17cd43f8e1147c6f0a
gordelenco <cristian.gordelenco@codwer.com>
2022-07-11 11:20:35 +0300

    Add Permision do Required Documents and Delete log

--- src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/RequiredDocuments/DeleteRequiredDocument/DeleteRequiredDocumentsCommand.cs
+++ src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/RequiredDocuments/DeleteRequiredDocument/DeleteRequiredDocumentsCommand.cs
@@ -1,7 +1,10 @@
-﻿using MediatR;
+﻿using CODWER.RERU.Evaluation.Application.Permissions;
+using CVU.ERP.Module.Application.Attributes;
+using MediatR;
 
 namespace CODWER.RERU.Evaluation.Application.RequiredDocuments.DeleteRequiredDocument
 {
+    [ModuleOperation(permission: PermissionCodes.ACCES_GENERAL_LA_SABLOANELE_DE_DOCUMENTE)]
     public class DeleteRequiredDocumentsCommand : IRequest<Unit>
     {
         public int Id { get; set; }
--- src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/RequiredDocuments/GetRequiredDocument/GetRequiredDocumentQuery.cs
+++ src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/RequiredDocuments/GetRequiredDocument/GetRequiredDocumentQuery.cs
@@ -1,8 +1,11 @@
-﻿using CODWER.RERU.Evaluation.DataTransferObjects.Documents;
+﻿using CODWER.RERU.Evaluation.Application.Permissions;
+using CODWER.RERU.Evaluation.DataTransferObjects.Documents;
+using CVU.ERP.Module.Application.Attributes;
 using MediatR;
 
 namespace CODWER.RERU.Evaluation.Application.RequiredDocuments.GetRequiredDocument
 {
+    [ModuleOperation(permission: PermissionCodes.ACCES_GENERAL_LA_SABLOANELE_DE_DOCUMENTE)]
     public class GetRequiredDocumentQuery : IRequest<RequiredDocumentDto>
     {
         public int Id { get; set; }
