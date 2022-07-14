e795b8dfa8d6a37e912fd99fa7261880d228fcb1
Pogor Cleopatra <cleopatra.pogor@codwer.com>
2022-07-14 18:01:35 +0300

    adjust vacantions page details

---
+++ src/Microservices/CODWER.RERU.Core/CODWER.RERU.Core.Application/CandidatePositions/CandidatePositionMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CODWER.RERU.Core.DataTransferObjects.CandidatePositions;
+using RERU.Data.Entities;
+
+namespace CODWER.RERU.Core.Application.CandidatePositions
+{
+    public class CandidatePositionMappingProfile : Profile
+    {
+        public CandidatePositionMappingProfile()
+        {
+            CreateMap<CandidatePosition, CandidatePositionDto>();
+        }
+    }
+}
---
+++ src/Microservices/CODWER.RERU.Core/CODWER.RERU.Core.DataTransferObjects/CandidatePositions/CandidatePositionDto.cs
@@ -0,0 +1,15 @@
+﻿using CVU.ERP.Common.DataTransferObjects.SelectValues;
+using System.Collections.Generic;
+
+namespace CODWER.RERU.Core.DataTransferObjects.CandidatePositions
+{
+    public class CandidatePositionDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public string Description { get; set; }
+        public List<SelectItem> RequiredDocuments { get; set; }
+        public List<SelectItem> Events { get; set; }
+    }
+}
