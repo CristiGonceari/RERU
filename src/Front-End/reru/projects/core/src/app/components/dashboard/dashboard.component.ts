65d6eaef94f0bb021f1ad6cb5af30cf56f218d58
cristian.gonceari@codwer.com <cristian.gonceari@codwer.com>
2022-07-14 13:10:34 +0300

    Try Fix undefined (reading 'map')

--- src/Front-End/reru/projects/core/src/app/components/dashboard/dashboard.component.ts
+++ src/Front-End/reru/projects/core/src/app/components/dashboard/dashboard.component.ts
@@ -53,7 +53,6 @@ export class DashboardComponent implements OnInit {
         {
           this.profileService.GetCandidateRegistrationSteps().subscribe(res => {
             if (res.data.unfinishedSteps.length != 0){
-              this.modules == null
               this.router.navigate(["./registration-flux",res.data.userProfileId,"step",res.data.unfinishedSteps[0]], { relativeTo: this.route });
             }
           })
