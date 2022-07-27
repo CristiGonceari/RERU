517e4c3d9057f9d6f1d34266945da70579106d3a
Pogor Cleopatra <cleopatra.pogor@codwer.com>
2022-07-27 17:56:02 +0300

    fix comment

--- src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/Tests/AddEvaluations/AddEvaluationsCommandHandler.cs
+++ src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/Tests/AddEvaluations/AddEvaluationsCommandHandler.cs
@@ -115,7 +115,7 @@ namespace CODWER.RERU.Evaluation.Application.Tests.AddEvaluations
                 userNames.Add(userProfile.FullName);
             }
 
-            return new StringBuilder( $@"<p style=""font-size: 22px; font-weight: 300;"">Ați fost invitat la evaluarea {testName} pentru {string.Join(",", userNames.ToArray())} în rol de evaluator.</p>").ToString();
+            return $@"<p style=""font-size: 22px; font-weight: 300;"">Ați fost invitat la evaluarea {testName} pentru {string.Join(",", userNames.ToArray())} în rol de evaluator.</p>";
         }
 
         private async Task LogAction(int testId)
