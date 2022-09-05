84dc7c290ef30168c32af0ea6a8839f21f6b2e38
cristian.gonceari@codwer.com <cristian.gonceari@codwer.com>
2022-09-05 15:50:35 +0300

    Fix task 10899 by adding validation and validation mesages

--- src/Microservices/CODWER.RERU.Core/CODWER.RERU.Core.Application/Studies/BulkAddEditStudies/BulkAddEditStudiesCommandValidation.cs
+++ src/Microservices/CODWER.RERU.Core/CODWER.RERU.Core.Application/Studies/BulkAddEditStudies/BulkAddEditStudiesCommandValidation.cs
@@ -1,4 +1,6 @@
-﻿using FluentValidation;
+﻿using CODWER.RERU.Core.Application.Validation;
+using CVU.ERP.Common.Validation;
+using FluentValidation;
 using RERU.Data.Persistence.Context;
 
 namespace CODWER.RERU.Core.Application.Studies.BulkAddEditStudies
@@ -9,6 +11,11 @@ namespace CODWER.RERU.Core.Application.Studies.BulkAddEditStudies
         {
             RuleForEach(x => x.Data)
                 .SetValidator(new StudyValidator(appDbContext));
+
+            RuleForEach(x => x.Data)
+               .Must(x => x.YearOfAdmission < x.GraduationYear)
+                .WithErrorCode(ValidationCodes.INVALID_TIME_RANGE);
+
         }
     }
 }
