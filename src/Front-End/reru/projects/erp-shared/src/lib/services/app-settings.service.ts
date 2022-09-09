19b186a850b49fd64a3dd08385c7d126bff22e1b
Vladislav Sireaev <vladislav.sireaev@codwer.com>
2022-09-09 15:52:20 +0300

    Add registration-flux responsive page

--- src/Front-End/reru/projects/erp-shared/src/lib/services/app-settings.service.ts
+++ src/Front-End/reru/projects/erp-shared/src/lib/services/app-settings.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpBackend } from '@angular/common/http';
 import { AppSettings, IAppSettings } from '../models/app-settings.model';
-//import { environment } from '../environments/environment';
+import { environment } from '../environments/environment';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class AppSettingsService {
 	private readonly http: HttpClient;
-	private readonly path = 'assets/config/appsettings.json';
+	private readonly path = environment.PRODUCTION ? 'assets/config/appsettings.json' : 'assets/config/appsettings-dev.json';
 	private appSettings: IAppSettings = new AppSettings();
 	constructor(handler: HttpBackend) {
 		console.log('===> init appsettings constructor');
