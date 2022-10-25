8a785a554757b147d5bf882381c57dfdce993c68
cristian.gonceari@codwer.com <cristian.gonceari@codwer.com>
2022-10-25 12:12:49 +0300

    no message

--- src/Front-End/reru/projects/core/src/app/components/users/edit/edit.component.ts
+++ src/Front-End/reru/projects/core/src/app/components/users/edit/edit.component.ts
@@ -168,18 +168,16 @@ export class EditComponent implements OnInit {
 					request.append('Data.File.Type', this.fileType.toString());
 					request.append('Data.UserId', res.data);
 					this.userService.addUserAvatar(request).subscribe(() => {
-						this.back();
 						this.notificationService.success(this.title, this.description, NotificationUtil.getDefaultMidConfig());
 						this.isLoading = false;
-						
+						this.back();
 					})
 				} else {
 					request.append('Data.UserId', res.data);
 					this.userService.addUserAvatar(request).subscribe(() => {
-						this.back();
 						this.notificationService.success(this.title, this.description, NotificationUtil.getDefaultMidConfig());
 						this.isLoading = false;
-						
+						this.back();
 					})
 				}
 			}
@@ -192,7 +190,6 @@ export class EditComponent implements OnInit {
 	}
 
 	back(): void {
-		// this.location.back();
-		this.router.navigate(['../../../users/list'], { relativeTo: this.activatedRoute });
+		this.location.back();
 	}
 }
