a858d9183c842d9124016791b6853c66c0c5d11f
Pogor Cleopatra <cleopatra.pogor@codwer.com>
2022-10-26 13:19:34 +0300

    add validation by current user at start test, get question, save answer

--- src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/Tests/StartEvaluation/StartEvaluationCommandValidator.cs
+++ src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/Tests/StartEvaluation/StartEvaluationCommandValidator.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CODWER.RERU.Evaluation.Application.Services;
+using CODWER.RERU.Evaluation.Application.Validation;
+using CODWER.RERU.Evaluation.Application.Validators.TestValidators;
+using CVU.ERP.Common.Data.Persistence.EntityFramework.Validators;
+using CVU.ERP.Common.Validation;
+using FluentValidation;
+using RERU.Data.Entities;
+using RERU.Data.Persistence.Context;
 
 namespace CODWER.RERU.Evaluation.Application.Tests.StartEvaluation
 {
-    class StartEvaluationCommandValidator
+    public class StartEvaluationCommandValidator : AbstractValidator<StartEvaluationCommand>
     {
+        public StartEvaluationCommandValidator(AppDbContext appDbContext, IUserProfileService userProfileService)
+        {
+            RuleFor(x => x.TestId)
+                .SetValidator(x => new ItemMustExistValidator<Test>(appDbContext, ValidationCodes.INVALID_TEST,
+                    ValidationMessages.InvalidReference));
+
+            RuleFor(x => x.TestId)
+                .SetValidator(x => new TestCurrentUserValidator(userProfileService, appDbContext, ValidationCodes.INVALID_USER));
+        }
     }
 }
