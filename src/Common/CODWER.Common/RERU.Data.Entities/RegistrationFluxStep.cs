0e6303c269c350184e58665d627b2ea79ba1d6b4
ANDRIAN-HOME-WORKSTATION <andrian.hubencu@codwer.com>
2022-10-28 18:17:00 +0300

    fix Registration flux migrations

--- src/Common/CODWER.Common/RERU.Data.Entities/RegistrationFluxStep.cs
+++ src/Common/CODWER.Common/RERU.Data.Entities/RegistrationFluxStep.cs
@@ -7,7 +7,7 @@ namespace RERU.Data.Entities
     public class RegistrationFluxStep : SoftDeleteBaseEntity
     {
         public bool IsDone { get; set; }
-        public bool InProgress { get; set; }
+        public bool? InProgress { get; set; }
         public RegistrationFluxStepEnum Step { get; set; }
         public int? ContractorId { get; set; }
         public Contractor Contractor { get; set; }
---
+++ src/Microservices/CODWER.RERU.Identity/CODWER.RERU.IdentityMigrator.Console/Migrations/AppDb/20221028151308_Fix_Nullable_Registration_Flux.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CODWER.RERU.IdentityMigrator.Console.Migrations.AppDb
+{
+    public partial class Fix_Nullable_Registration_Flux : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "InProgress",
+                table: "RegistrationFluxSteps",
+                type: "boolean",
+                nullable: true,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "InProgress",
+                table: "RegistrationFluxSteps",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+        }
+    }
+}
