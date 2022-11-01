13a05e85a0e85eda727a640660e0dbeb69e4a4b1
gordelenco <cristian.gordelenco@codwer.com>
2022-11-01 14:39:49 +0200

    Add Wrong old Password Validator

---
+++ src/Microservices/CODWER.RERU.Core/CODWER.RERU.Core.Application/Users/ChangeMyPassword/ChangeMyPasswordCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using CODWER.RERU.Core.Application.Validation;
+using CODWER.RERU.Core.Data.Persistence.Context;
+using CVU.ERP.Identity.Models;
+using CVU.ERP.ServiceProvider;
+using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace CODWER.RERU.Core.Application.Users.ChangeMyPassword
+{
+    public class ChangeMyPasswordCommandValidator : AbstractValidator<ChangeMyPasswordCommand>
+    {
+        public ChangeMyPasswordCommandValidator(UserManager<ERPIdentityUser> userManager, ICurrentApplicationUserProvider userProvider, UserManagementDbContext userManagementDbContext)
+        {
+            var currentUser = userProvider.Get().Result;
+            var user = userManagementDbContext.Users.FirstOrDefaultAsync(u => u.Email == currentUser.Email).Result;
+
+            RuleFor(x => x.oldPassword)
+                .Must(x => userManager.CheckPasswordAsync(user, x).Result)
+                .WithErrorCode(ValidationCodes.WRONG_OLD_PASSWORD);
+        }
+    }
+}
