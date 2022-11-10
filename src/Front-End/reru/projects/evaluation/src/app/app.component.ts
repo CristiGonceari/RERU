b425d3b274737d262ca19dfe0fe9af446593818b
gordelenco <cristian.gordelenco@codwer.com>
2022-11-10 15:49:58 +0200

    FIx Solicited Postiions Name On FE, Change Location on SideBar, CHange Names on FE

--- src/Front-End/reru/projects/evaluation/src/app/app.component.ts
+++ src/Front-End/reru/projects/evaluation/src/app/app.component.ts
@@ -130,19 +130,6 @@ export class AppComponent {
 			name: '',
 		},
 		{
-			permission: 'P03001101',
-			type: SidebarItemType.ITEM,
-			url: '/solicited-tests',
-			name: '',
-			icon: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-			<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-				<rect x="0" y="0" width="24" height="24"/>
-				<path d="M12.2674799,18.2323597 L12.0084872,5.45852451 C12.0004303,5.06114792 12.1504154,4.6768183 12.4255037,4.38993949 L15.0030167,1.70195304 L17.5910752,4.40093695 C17.8599071,4.6812911 18.0095067,5.05499603 18.0083938,5.44341307 L17.9718262,18.2062508 C17.9694575,19.0329966 17.2985816,19.701953 16.4718324,19.701953 L13.7671717,19.701953 C12.9505952,19.701953 12.2840328,19.0487684 12.2674799,18.2323597 Z" fill="#000000" fill-rule="nonzero" transform="translate(14.701953, 10.701953) rotate(-135.000000) translate(-14.701953, -10.701953) "/>
-				<path d="M12.9,2 C13.4522847,2 13.9,2.44771525 13.9,3 C13.9,3.55228475 13.4522847,4 12.9,4 L6,4 C4.8954305,4 4,4.8954305 4,6 L4,18 C4,19.1045695 4.8954305,20 6,20 L18,20 C19.1045695,20 20,19.1045695 20,18 L20,13 C20,12.4477153 20.4477153,12 21,12 C21.5522847,12 22,12.4477153 22,13 L22,18 C22,20.209139 20.209139,22 18,22 L6,22 C3.790861,22 2,20.209139 2,18 L2,6 C2,3.790861 3.790861,2 6,2 L12.9,2 Z" fill="#000000" fill-rule="nonzero" opacity="0.3"/>
-			</g>
-		</svg>`,
-		},
-		{
 			permission: 'P03000601',
 			type: SidebarItemType.ITEM,
 			url: '/tests',
@@ -271,6 +258,25 @@ export class AppComponent {
 			name: '',
 		},
 		{
+			permission: 'P03001101',
+			type: SidebarItemType.ITEM,
+			url: '/solicited-tests',
+			name: '',
+			icon: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+			<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+				<rect x="0" y="0" width="24" height="24"/>
+				<path d="M8,3 L8,3.5 C8,4.32842712 8.67157288,5 9.5,5 L14.5,5 C15.3284271,5 16,4.32842712 16,3.5 L16,3 L18,3 C19.1045695,3 20,3.8954305 20,5 L20,21 C20,22.1045695 19.1045695,23 18,23 L6,23 C4.8954305,23 4,22.1045695 4,21 L4,5 C4,3.8954305 4.8954305,3 6,3 L8,3 Z" fill="#000000" opacity="0.3"/>
+				<path d="M11,2 C11,1.44771525 11.4477153,1 12,1 C12.5522847,1 13,1.44771525 13,2 L14.5,2 C14.7761424,2 15,2.22385763 15,2.5 L15,3.5 C15,3.77614237 14.7761424,4 14.5,4 L9.5,4 C9.22385763,4 9,3.77614237 9,3.5 L9,2.5 C9,2.22385763 9.22385763,2 9.5,2 L11,2 Z" fill="#000000"/>
+				<rect fill="#000000" opacity="0.3" x="10" y="9" width="7" height="2" rx="1"/>
+				<rect fill="#000000" opacity="0.3" x="7" y="9" width="2" height="2" rx="1"/>
+				<rect fill="#000000" opacity="0.3" x="7" y="13" width="2" height="2" rx="1"/>
+				<rect fill="#000000" opacity="0.3" x="10" y="13" width="7" height="2" rx="1"/>
+				<rect fill="#000000" opacity="0.3" x="7" y="17" width="2" height="2" rx="1"/>
+				<rect fill="#000000" opacity="0.3" x="10" y="17" width="7" height="2" rx="1"/>
+			</g>
+		</svg>`,
+		},
+		{
 			permission: 'P03001201',
 			type: SidebarItemType.ITEM,
 			url: '/positions',
@@ -359,7 +365,6 @@ export class AppComponent {
 			this.translate.get('sidebar.manage-questions'),
 			this.translate.get('sidebar.manage-tests'),
 			this.translate.get('sidebar.test'),
-			this.translate.get('solicited-position.solicited-positions'),
 			this.translate.get('verify-test.title'),
 			this.translate.get('evaluations.evaluations'),
 			this.translate.get('statistics.statistics'),
@@ -370,11 +375,12 @@ export class AppComponent {
 			this.translate.get('sidebar.documents-management'),
 			this.translate.get('sidebar.documents-template'),
 			this.translate.get('sidebar.administration'),
+			this.translate.get('solicited-position.solicited-positions'),
 			this.translate.get('sidebar.positions'),
 			this.translate.get('sidebar.required-documents'),
 			this.translate.get('faq.help'),
 			this.translate.get('faq.faq'),
-		]).subscribe(([home, activities, settings, categories, questions, tests, test, solicitedTest, verifyTest, evaluations, statistic, event, location, events, plan, documentsManagement, documentsTemplate, administration, position, requiredDocuments, help, faq]) => {
+		]).subscribe(([home, activities, settings, categories, questions, tests, test,  verifyTest, evaluations, statistic, event, location, events, plan, documentsManagement, documentsTemplate, administration,solicitedTest, position, requiredDocuments, help, faq]) => {
 			this.sidebarItems[0].name = home;
 			this.sidebarItems[1].name = activities;
 			this.sidebarItems[2].name = settings;
@@ -382,17 +388,17 @@ export class AppComponent {
 			this.sidebarItems[4].name = questions;
 			this.sidebarItems[5].name = tests;
 			this.sidebarItems[6].name = test;
-			this.sidebarItems[7].name = solicitedTest;
-			this.sidebarItems[8].name = verifyTest;
-			this.sidebarItems[9].name = evaluations;
-			this.sidebarItems[10].name = statistic;
-			this.sidebarItems[11].name = event;
-			this.sidebarItems[12].name = location;
-			this.sidebarItems[13].name = events;
-			this.sidebarItems[14].name = plan;
-			this.sidebarItems[15].name = documentsManagement;
-			this.sidebarItems[16].name = documentsTemplate;
-			this.sidebarItems[17].name = administration;
+			this.sidebarItems[7].name = verifyTest;
+			this.sidebarItems[8].name = evaluations;
+			this.sidebarItems[9].name = statistic;
+			this.sidebarItems[10].name = event;
+			this.sidebarItems[11].name = location;
+			this.sidebarItems[12].name = events;
+			this.sidebarItems[13].name = plan;
+			this.sidebarItems[14].name = documentsManagement;
+			this.sidebarItems[15].name = documentsTemplate;
+			this.sidebarItems[16].name = administration;
+			this.sidebarItems[17].name = solicitedTest;
 			this.sidebarItems[18].name = position;
 			this.sidebarItems[19].name = requiredDocuments;
 			this.sidebarItems[20].name = help;
