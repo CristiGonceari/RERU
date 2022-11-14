6740b8d7a2d3a45300e0592fdcf5584acd3478d3
gordelenco <cristian.gordelenco@codwer.com>
2022-11-14 13:46:29 +0200

    Refactor aprove-solicited-position Fe component, for optimizing requests, change url from Navigate from rigth-sidebar

--- src/Front-End/reru/projects/evaluation/src/app/components/solicited-tests/solicited-test-list/approve-solicited-test/approve-solicited-test.component.ts
+++ src/Front-End/reru/projects/evaluation/src/app/components/solicited-tests/solicited-test-list/approve-solicited-test/approve-solicited-test.component.ts
@@ -1,15 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { TestTemplateStatusEnum } from 'projects/evaluation/src/app/utils/enums/test-template-status.enum';
-import { SelectItem } from 'projects/evaluation/src/app/utils/models/select-item.model';
 import { I18nService } from 'projects/evaluation/src/app/utils/services/i18n/i18n.service';
-import { ReferenceService } from 'projects/evaluation/src/app/utils/services/reference/reference.service';
-import { TestTemplateService } from 'projects/evaluation/src/app/utils/services/test-template/test-template.service';
 import { SolicitedTest } from 'projects/evaluation/src/app/utils/models/solicitet-tests/solicited-test.model';
 import { Location } from '@angular/common';
 import { SolicitedTestService } from 'projects/evaluation/src/app/utils/services/solicited-test/solicited-test.service';
 import { ActivatedRoute } from '@angular/router';
-import { AddEditTest } from 'projects/evaluation/src/app/utils/models/tests/add-edit-test.model';
-import { TestStatusEnum } from 'projects/evaluation/src/app/utils/enums/test-status.enum';
 import { SolicitedVacantPositionUserFileService } from 'projects/evaluation/src/app/utils/services/solicited-vacant-position-user-file/solicited-vacant-position-user-file.service';
 import { saveAs } from 'file-saver';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -22,62 +16,36 @@ import { SolicitedVacantPositionEmailMessageService } from 'projects/evaluation/
   styleUrls: ['./approve-solicited-test.component.scss']
 })
 export class ApproveSolicitedTestComponent implements OnInit {
-  eventsList: any;
-  selectActiveTests: any;
-  evaluatorList: any;
-
-  user = new SelectItem();
-  event = new SelectItem();
-  testTemplate = new SelectItem();
-  evaluator = new SelectItem();
-
   date: Date;
-  search: string;
   title: string;
   description: string;
-  isLoading: boolean = true;
   solicitedTestId;
-  solicitedTest: SolicitedTest = new SolicitedTest();
-
-  showName: boolean = false;
-  isTestTemplateOneAnswer: boolean = false;
-  printTest: boolean = true;
-  needEvaluator: boolean = false;
-  hasEventEvaluator: boolean = false;
-
   candidatePositionId;
 
-  messageText = "";
+  solicitedTest: SolicitedTest = new SolicitedTest();
 
-  userListToAdd: number[] = [];
+  isLoading: boolean = true;
+  isLoadingMedia: boolean = true;
+
+  messageText = "";
 
   userFiles: any[] = [];
 
   constructor(
-    private referenceService: ReferenceService,
     private solicitedTestService: SolicitedTestService,
     public translate: I18nService,
     private location: Location,
     public activatedRoute: ActivatedRoute,
     private modalService: NgbModal,
-    private testTemplateService: TestTemplateService,
     private solicitedVacantPositionUserFileService: SolicitedVacantPositionUserFileService,
     private solicitedVacantPositionEmailMessageService: SolicitedVacantPositionEmailMessageService
   ) { }
 
   ngOnInit(): void {
-    this.getEvaluators();
-    this.getEvents();
+    this.initData();
   }
 
-  getEvents() {
-    this.referenceService.getEvents().subscribe(res => {
-      this.eventsList = res.data;
-      this.getActiveTestTemplate();
-    });
-  }
-
-  GetFile(fileId: string) {
+  getFile(fileId: string) {
     this.solicitedVacantPositionUserFileService.get(fileId).subscribe(response => {
       if (response) {
         const fileName = response.headers.get('Content-Disposition').split("filename=")[1].split(';')[0];
@@ -93,99 +61,45 @@ export class ApproveSolicitedTestComponent implements OnInit {
   initData(): void {
     this.solicitedTestId = this.activatedRoute.snapshot.paramMap.get('id');
     this.candidatePositionId = this.activatedRoute.snapshot.paramMap.get('positionId');
-    if (this.solicitedTestId != null) this.getSolicitedTest(this.solicitedTestId, this.candidatePositionId)
+    if (this.solicitedTestId != null) this.getSolicitedPosition(this.solicitedTestId, this.candidatePositionId)
     else this.isLoading = false;
   }
 
-  getSolicitedTest(id, positionId) {
-    this.solicitedTestService.getSolicitedTest({ id: id, candidatePositionId: positionId }).subscribe(res => {
-      if (res && res.data) {
-        this.solicitedTest = res.data;
-        this.date = res.data.solicitedTime;
-        this.isLoading = false;
-        this.checkIfEventHasEvaluator(res.data.eventId);
-        this.checkIfIsOneAnswer(res.data.testTemplateId)
-        this.userListToAdd.push(res.data.userProfileId);
-      }
-      this.solicitedVacantPositionUserFileService.getList({ userId: this.solicitedTest.userProfileId, solicitedVacantPositionId: this.solicitedTestId, candidatePositionId: this.solicitedTest.candidatePositionId }).subscribe(res => {
-        this.userFiles = res.data
-      })
-    });
+  getSolicitedPosition(id, positionId) {
+    this.solicitedTestService.getSolicitedTest({ id: id, candidatePositionId: positionId })
+      .subscribe(res => {
+        if (res && res.data) {
+          this.solicitedTest = res.data;
+          this.date = res.data.solicitedTime;
+          this.isLoading = false;
+        }
 
+        this.solicitedVacantPositionUserFileService
+          .getList(this.parse(this.solicitedTest.userProfileId, this.solicitedTestId, this.solicitedTest.candidatePositionId))
+          .subscribe(res => {
+            this.userFiles = res.data;
+            this.isLoadingMedia = false;
+          })
+      });
   }
 
-  getActiveTestTemplate() {
-    let params = {
-      testTemplateStatus: TestTemplateStatusEnum.Active
+  parse(userId: number, solicitedVacantPositionId: number, candidatePositionId: number) {
+    let request = {
+      userId,
+      solicitedVacantPositionId,
+      candidatePositionId
     }
 
-    this.testTemplateService.getTestTemplateByStatus(params).subscribe((res) => {
-      this.selectActiveTests = res.data;
-      this.initData();
-    })
+    return request
   }
 
-  getEvaluators() {
-    if (this.needEvaluator === false && this.hasEventEvaluator === false) {
-      this.referenceService.getUsers({ eventId: this.event.value }).subscribe(res => { this.evaluatorList = res.data });
-    } else {
-      this.evaluatorList = [];
-      this.evaluator.value = null;
-    };
-  }
-
-  checkIfIsOneAnswer(testTemplateId) {
-    if (testTemplateId) {
-      this.isTestTemplateOneAnswer = this.selectActiveTests.find(x => x.testTemplateId === testTemplateId).isOnlyOneAnswer;
-    } else {
-      this.isTestTemplateOneAnswer = false;
-    }
-
-    if (!this.printTest)
-      this.messageText = "Acest test poate conține video sau audio!"
-
-    if (this.isTestTemplateOneAnswer) {
-      this.evaluator.value = null;
-    }
-  }
-
-  onItemChange(event) {
-    this.showName = event.target.checked;
-  }
-
-  checkIfEventHasEvaluator(event) {
-    if (event) {
-      this.hasEventEvaluator = this.eventsList.find(x => x.eventId === event).isEventEvaluator;
-    }
-  }
-
-  setTimeToSearch(): void {
-    if (this.date) {
-      const date = new Date(this.date);
-      this.search = new Date(date.getTime() - (new Date(this.date).getTimezoneOffset() * 60000)).toISOString();
-    }
-  }
-
-  parse() {
-    this.setTimeToSearch();
-    return new AddEditTest({
-      userProfileId: this.userListToAdd,
-      programmedTime: this.search,
-      eventId: this.solicitedTest.eventId || null,
-      evaluatorId: +this.evaluator.value || null,
-      testStatus: TestStatusEnum.Programmed,
-      testTemplateId: this.solicitedTest.testTemplateId || 0,
-      showUserName: this.showName
-    })
-  }
-
-  createTest() {
+  next() {
     const modalRef: any = this.modalService.open(ReviewSolicitedVacandPositionModalComponent, { centered: true, size: 'xl' });
     modalRef.componentInstance.userEmail = this.solicitedTest.email;
     modalRef.componentInstance.userName = this.solicitedTest.userProfileName;
     modalRef.componentInstance.solicitedTestId = this.solicitedTestId;
     modalRef.result.then(() => {
-      this.sendEmailAndChangeStatus( modalRef.result.__zone_symbol__value)
+      this.sendEmailAndChangeStatus(modalRef.result.__zone_symbol__value)
     }, () => { });
   }
 
@@ -196,9 +110,7 @@ export class ApproveSolicitedTestComponent implements OnInit {
       solicitedVacantPositionId: this.solicitedTestId
     }
 
-    this.solicitedVacantPositionEmailMessageService.changeStatusAndSendEmail(request).subscribe(res => {
-      this.backClicked();
-    })
+    this.solicitedVacantPositionEmailMessageService.changeStatusAndSendEmail(request).subscribe(() => this.backClicked());
   }
 
   ceckFileNameLength(fileName: string) {
