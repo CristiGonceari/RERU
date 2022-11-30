e2dea99013fba9e69f0e474906880b5e4ffe94db
gordelenco <cristian.gordelenco@codwer.com>
2022-11-30 17:53:48 +0200

    Add link To Candidate position From Solicited Positions, fix AttachUserModal chech box,  Save and GetTestQuesiton by mooving with pagination, replace {user_name_key] in email message, fix buttons from start test with confirmation

--- src/Front-End/reru/projects/evaluation/src/app/components/tests/one-per-page-performing-test/one-per-page-performing-test.component.ts
+++ src/Front-End/reru/projects/evaluation/src/app/components/tests/one-per-page-performing-test/one-per-page-performing-test.component.ts
@@ -17,7 +17,7 @@ import { TestTemplateSettings } from '../../../utils/models/test-templates/test-
 import { AddTestQuestion } from '../../../utils/models/test-questions/add-test-question.model';
 import { SafeHtmlPipe } from '../../../utils/pipes/safe-html.pipe';
 import { ConfirmModalComponent } from '@erp/shared';
-import { forkJoin } from 'rxjs';
+import { forkJoin, Observable } from 'rxjs';
 import { I18nService } from '../../../utils/services/i18n/i18n.service';
 import { FileTestAnswerService } from '../../../utils/services/FileTestAnswer/file-test-answer.service';
 import { saveAs } from 'file-saver';
@@ -247,6 +247,17 @@ export class OnePerPagePerformingTestComponent implements OnInit {
     }
   }
 
+  checkSaveAnswer(index) {
+      let isCkecked: boolean = false;
+      isCkecked = this.testOptionsList.some((x) => x.isSelected == true); 
+
+       if (isCkecked) {
+        this.saveAnswers();
+       } else {
+        this.getTestQuestions(index)
+      }
+  }
+
   saveAnswers() {
     this.disableBtn = true;
     this.isLoading = true;
@@ -421,6 +432,7 @@ export class OnePerPagePerformingTestComponent implements OnInit {
   }
 
   getTestQuestions(questionIndex?: number) {
+
     this.isLoading = true;
     this.questionIndex = questionIndex == null ? this.questionIndex : questionIndex;
 
