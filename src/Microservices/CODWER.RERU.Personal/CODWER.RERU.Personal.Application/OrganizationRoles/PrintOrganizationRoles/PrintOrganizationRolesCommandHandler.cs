ce653b94d1c5c8f7f06fa47cddc996634d698430
gordelenco <cristian.gordelenco@codwer.com>
2023-01-18 18:44:53 +0200

    Add Print Roles Table On Personal

---
+++ src/Microservices/CODWER.RERU.Personal/CODWER.RERU.Personal.Application/OrganizationRoles/PrintOrganizationRoles/PrintOrganizationRolesCommandHandler.cs
@@ -0,0 +1,52 @@
+﻿using CODWER.RERU.Personal.DataTransferObjects.OrganizationRoles;
+using CVU.ERP.Common.DataTransferObjects.Files;
+using CVU.ERP.Module.Application.TableExportServices;
+using MediatR;
+using RERU.Data.Entities.PersonalEntities;
+using RERU.Data.Persistence.Context;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CODWER.RERU.Personal.Application.OrganizationRoles.PrintOrganizationRoles
+{
+    public class PrintOrganizationRolesCommandHandler : IRequestHandler<PrintOrganizationRolesCommand, FileDataDto>
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly IExportData<Role, RoleDto> _printer;
+
+        public PrintOrganizationRolesCommandHandler(AppDbContext appDbContext, IExportData<Role, RoleDto> printer)
+        {
+            _appDbContext = appDbContext;
+            _printer = printer;
+        }
+
+        public async Task<FileDataDto> Handle(PrintOrganizationRolesCommand request, CancellationToken cancellationToken)
+        {
+            var items = _appDbContext.Roles
+                .OrderBy(x => x.Name)
+                .AsQueryable();
+
+            if (!string.IsNullOrEmpty(request.SearchWord))
+            {
+                items = items.Where(x => x.Name.ToLower().Contains(request.SearchWord.ToLower()));
+            }
+
+            if (!string.IsNullOrEmpty(request.Name))
+            {
+                items = items.Where(x => x.Name.ToLower().Contains(request.Name.ToLower()));
+            }
+
+            var result = _printer.ExportTableSpecificFormat(new TableData<Role>
+            {
+                Name = request.TableName,
+                Items = items,
+                Fields = request.Fields,
+                Orientation = request.Orientation,
+                ExportFormat = request.TableExportFormat
+            });
+
+            return result;
+        }
+    }
+}
---
+++ src/Microservices/CODWER.RERU.Personal/CODWER.RERU.Personal.Application/OrganizationRoles/PrintOrganizationRoles/PrintOrganizationRolesCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using CODWER.RERU.Personal.Application.Validation;
+using CODWER.RERU.Personal.DataTransferObjects.OrganizationRoles;
+using CVU.ERP.Common.Validation;
+using CVU.ERP.Module.Application.TableExportServices.Validators;
+using FluentValidation;
+using System.Linq;
+
+namespace CODWER.RERU.Personal.Application.OrganizationRoles.PrintOrganizationRoles
+{
+    public class PrintOrganizationRolesCommandValidator : AbstractValidator<PrintOrganizationRolesCommand>
+    {
+        public PrintOrganizationRolesCommandValidator()
+        {
+            RuleFor(x => x.Fields.Select(x => x.Value).ToList())
+                .SetValidator(new TableExporterValidator<RoleDto>(ValidationMessages.InvalidInput, ValidationCodes.INVALID_INPUT));
+        }
+    }
+}
