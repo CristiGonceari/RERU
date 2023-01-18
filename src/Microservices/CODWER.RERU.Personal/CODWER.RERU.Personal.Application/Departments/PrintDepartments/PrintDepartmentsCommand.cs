ece46234ee8e5510fa79627369141c10bc2bdb74
gordelenco <cristian.gordelenco@codwer.com>
2023-01-18 18:11:33 +0200

    Print Departments Table Personal

---
+++ src/Microservices/CODWER.RERU.Personal/CODWER.RERU.Personal.Application/Departments/PrintDepartments/PrintDepartmentsCommand.cs
@@ -0,0 +1,15 @@
+﻿using CODWER.RERU.Personal.Application.Permissions;
+using CVU.ERP.Common.DataTransferObjects.Files;
+using CVU.ERP.Module.Application.Attributes;
+using CVU.ERP.Module.Application.TableExportServices;
+using MediatR;
+
+namespace CODWER.RERU.Personal.Application.Departments.PrintDepartments
+{
+    [ModuleOperation(permission: PermissionCodes.ACCESS_GENERAL_LA_DEPARTAMENTE)]
+
+    public class PrintDepartmentsCommand : TableParameter, IRequest<FileDataDto>
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/Microservices/CODWER.RERU.Personal/CODWER.RERU.Personal.Application/Departments/PrintDepartments/PrintDepartmentsCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using CODWER.RERU.Personal.DataTransferObjects.Departments;
+using CVU.ERP.Common.DataTransferObjects.Files;
+using CVU.ERP.Module.Application.TableExportServices;
+using MediatR;
+using RERU.Data.Entities.PersonalEntities;
+using RERU.Data.Persistence.Context;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CODWER.RERU.Personal.Application.Departments.PrintDepartments
+{
+    public class PrintDepartmentsCommandHandler : IRequestHandler<PrintDepartmentsCommand, FileDataDto>
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly IExportData<Department, DepartmentDto> _printer;
+
+
+        public PrintDepartmentsCommandHandler(AppDbContext appDbContext, IExportData<Department, DepartmentDto> printer)
+        {
+            _appDbContext = appDbContext;
+            _printer = printer;
+        }
+
+        public async Task<FileDataDto> Handle(PrintDepartmentsCommand request, CancellationToken cancellationToken)
+        {
+            var items = _appDbContext.Departments
+                .AsQueryable();
+
+            if (!string.IsNullOrEmpty(request.Name))
+            {
+                items = items.Where(x => x.Name.ToLower().Contains(request.Name.ToLower()));
+            }
+
+            var result = _printer.ExportTableSpecificFormat(new TableData<Department>
+            {
+                Name = request.TableName,
+                Items = items,
+                Fields = request.Fields,
+                Orientation = request.Orientation,
+                ExportFormat = request.TableExportFormat
+            });
+
+            return result;
+        }
+    }
+}
