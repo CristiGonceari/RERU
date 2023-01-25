0ee62d07a0821234ce4c721af15a04d33de308e6
Alexandru Tocan <alexandru.tocan@codwer.com>
2023-01-25 12:09:52 +0200

    Change Email Notification

--- src/Microservices/CODWER.RERU.Evaluation360/CODWER.RERU.Evaluation360.Application/BLL/Evaluations/Reject/RejectEvaluationCommandHandler.cs
+++ src/Microservices/CODWER.RERU.Evaluation360/CODWER.RERU.Evaluation360.Application/BLL/Evaluations/Reject/RejectEvaluationCommandHandler.cs
@@ -52,7 +52,7 @@ namespace CODWER.RERU.Evaluation360.Application.BLL.Evaluations.Reject
 
             await _notificationService.PutEmailInQueue(new QueuedEmailData
             {
-                Subject = "Respins de evaluat",
+                Subject = "Respingerea Evaluarii de performanță",
                 To = evaluator.Email,
                 HtmlTemplateAddress = "Templates/Evaluation/EmailNotificationTemplate.html",
                 ReplacedValues = new Dictionary<string, string>()
