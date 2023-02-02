82b24547fdecfaf65e68a55618418f211ac896da
Evghenia Ivanova <evghenia.ivanova@codwer.com>
2023-02-02 09:51:49 +0200

    added compenent help for ev360

---
+++ src/Microservices/CODWER.RERU.Evaluation360/CODWER.RERU.Evaluation360.Application/BLL/Articles/GetArticle/GetArticleQueryHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using CODWER.RERU.Core.DataTransferObjects.Articles;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+using CVU.ERP.Common.DataTransferObjects.SelectValues;
+using RERU.Data.Entities;
+using RERU.Data.Persistence.Context;
+
+namespace CODWER.RERU.Evaluation360.Application.BLL.Articles.GetArticle
+{
+    public class GetArticleQueryHandler : IRequestHandler<GetArticleQuery, ArticleCoreDto>
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly IMapper _mapper;
+
+        public GetArticleQueryHandler(AppDbContext appDbContext, IMapper mapper)
+        {
+            _appDbContext = appDbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<ArticleCoreDto> Handle(GetArticleQuery request, CancellationToken cancellationToken)
+        {
+            var article = await _appDbContext.CoreArticles.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            var mappedItem = _mapper.Map<ArticleCoreDto>(article);
+
+            mappedItem.Roles = await GetRoles(article.Id);
+
+            return mappedItem;
+        }
+
+        private async Task<List<SelectItem>> GetRoles(int articleId)
+        {
+            return await _appDbContext.ArticleCoreModuleRoles
+                .Include(x => x.Role)
+                .Where(x => x.ArticleId == articleId)
+                .Select(x => new ModuleRole()
+                {
+                    Id = x.Role.Id,
+                    Name = x.Role.Name
+                })
+                .Select(e => _mapper.Map<SelectItem>(e))
+                .ToListAsync();
+        }
+    }
+}
---
+++ src/Microservices/CODWER.RERU.Evaluation360/CODWER.RERU.Evaluation360.Application/BLL/Articles/PrintArticles/PrintArticlesCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using CVU.ERP.Common.DataTransferObjects.Files;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using CODWER.RERU.Core.DataTransferObjects.Articles;
+using CVU.ERP.Module.Application.TableExportServices;
+using CVU.ERP.ServiceProvider;
+using RERU.Data.Persistence.Context;
+using RERU.Data.Entities;
+
+namespace CODWER.RERU.Evaluation360.Application.BLL.Articles.PrintArticles
+{
+    public class PrintArticlesCommandHandler : IRequestHandler<PrintArticlesCommand, FileDataDto>
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly IExportData<ArticleCore, ArticleCoreDto> _printer;
+        private readonly ICurrentApplicationUserProvider _currentApplication;
+
+        public PrintArticlesCommandHandler(AppDbContext appDbContext, IExportData<ArticleCore, ArticleCoreDto> printer, ICurrentApplicationUserProvider currentApplication)
+        {
+            _appDbContext = appDbContext;
+            _printer = printer;
+            _currentApplication = currentApplication;
+        }
+
+        public async Task<FileDataDto> Handle(PrintArticlesCommand request, CancellationToken cancellationToken)
+        {
+            var currentUser = await _currentApplication.Get();
+
+            var articles = GetAndFilterArticles.Filter(_appDbContext, request.Name, currentUser);
+
+            var result = _printer.ExportTableSpecificFormat(new TableData<ArticleCore>
+            {
+                Name = request.TableName,
+                Items = articles,
+                Fields = request.Fields,
+                Orientation = request.Orientation,
+                ExportFormat = request.TableExportFormat
+            });
+
+            return result;
+        }
+    }
+}
