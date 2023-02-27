8676ff1c1814e70740cb6671ab091fae0337c8c1
Alexandru Tocan <alexandru.tocan@codwer.com>
2023-02-27 18:23:47 +0200

    When exporting documents from the user profile, in pdf table is not populated

---
+++ src/Microservices/CODWER.RERU.Core/CODWER.RERU.Core.Application/UserFiles/PrintUserFiles/PrintUserFilesValidator.cs
@@ -0,0 +1,14 @@
+﻿using CODWER.RERU.Core.Application.Validation;
+using CODWER.RERU.Core.DataTransferObjects.Files;
+using CVU.ERP.Common.Validation;
+using CVU.ERP.Module.Application.TableExportServices.Validators;
+using FluentValidation;
+using System.Linq;
+
+namespace CODWER.RERU.Core.Application.UserFiles.PrintUserFiles
+{
+    public class PrintUserFilesValidator : AbstractValidator<PrintUserFilesCommand>
+    {
+        
+    }
+}
