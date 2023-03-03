75dd67d9158fb23ec0833a935d869d013b90fbe7
Alexandru Tocan <alexandru.tocan@codwer.com>
2023-03-03 16:33:57 +0200

    Add info for user (max 3000 rows) to all modals

--- src/Front-End/reru/projects/evaluation360/src/app/components/faq/faq-list/faq-list-table/faq-list-table.component.ts
+++ src/Front-End/reru/projects/evaluation360/src/app/components/faq/faq-list/faq-list-table/faq-list-table.component.ts
@@ -79,12 +79,14 @@ export class FaqListTableComponent implements OnInit {
 	}
 
 	translateData(): void {
-		this.printTranslates = ['print-table', 'print-msg', 'sorted-by', 'cancel']
+		this.printTranslates = ['print-table', 'print-msg', 'sorted-by', 'cancel', 'select-file-format', 'max-print-rows']
 		forkJoin([
 			this.translate.get('print.print-table'),
 			this.translate.get('print.print-msg'),
 			this.translate.get('print.sorted-by'),
-			this.translate.get('button.cancel')
+			this.translate.get('button.cancel'),
+			this.translate.get('print.select-file-format'),
+			this.translate.get('print.max-print-rows')
 		]).subscribe(
 			(items) => {
 				for (let i=0; i<this.printTranslates.length; i++) {
