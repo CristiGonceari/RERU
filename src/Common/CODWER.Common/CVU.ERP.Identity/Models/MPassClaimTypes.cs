59ef82a00560a85ac3f640bab71f320d5aab6661
cristian.gonceari@codwer.com <cristian.gonceari@codwer.com>
2023-03-10 18:49:12 +0200

    Try add MPass authentification

---
+++ src/Common/CODWER.Common/CVU.ERP.Identity/Models/MPassClaimTypes.cs
@@ -0,0 +1,14 @@
+﻿namespace CVU.ERP.Identity.Models
+{
+    public static class MPassClaimTypes
+    {
+        public const string UserName = "Username";
+        public const string SessionIndex = "SessionIndex";
+        public const string FirstName = "FirstName";
+        public const string LastName = "LastName";
+        public const string BirthDate = "BirthDate";
+        public const string PhoneNumber = "family_name";
+        public const string EmailAdress = "EmailAddress";
+
+    }
+}
