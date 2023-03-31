9778deaeae932f2ecc43367d5e8e96c59ed16d64
Diana Ribac <diana.ribac@codwer.com>
2023-03-31 16:23:29 +0300

    add translates for personal and evaluation module

--- src/Front-End/reru/projects/personal/src/app/utils/modals/add-photo-modal/add-photo-modal.component.ts
+++ src/Front-End/reru/projects/personal/src/app/utils/modals/add-photo-modal/add-photo-modal.component.ts
@@ -4,6 +4,9 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { NotificationsService } from 'angular2-notifications';
 import { ContractorService } from '../../services/contractor.service';
 import { NotificationUtil } from '../../util/notification.util';
+import { I18nService } from '../../services/i18n.service';
+import { forkJoin, Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-add-photo-modal',
@@ -20,45 +23,55 @@ export class AddPhotoModalComponent implements OnInit {
   fileType: string = null;
   attachedFile: File;
   
-  constructor(private activeModal: NgbActiveModal,
+  constructor(
+    private activeModal: NgbActiveModal,
     private contractorService: ContractorService,
     private notificationService: NotificationsService,
-              private fb: FormBuilder) { }
+    private fb: FormBuilder,
+    public translate: I18nService,
+    ) { }
+    
+    ngOnInit(): void {
+      this.initForm();
+      this.isLoading = false;
+      }
+      
+    initForm(): void {
+      this.uploadForm = this.fb.group({
+        file: this.fb.control(null, [Validators.required])
+      })
+    }
+    
+    dismiss(): void {
+      this.activeModal.dismiss();
+    }
+    
+    addPhoto() {
+      const request = new FormData();
+      
+      if (this.attachedFile) {
+        this.fileType = '7';
+        request.append('Data.FileDto.File', this.attachedFile);
+        request.append('Data.FileDto.Type', this.fileType);
+      }
+      request.append('Data.ContractorId', this.contractorId);
+      request.append('Data.MediaFileId', this.uploadForm.value.mediaFileId);
 
-              ngOnInit(): void {
-                this.initForm();
-                this.isLoading = false;
-              }
-            
-              initForm(): void {
-                this.uploadForm = this.fb.group({
-                  file: this.fb.control(null, [Validators.required])
-                })
-              }
-            
-              dismiss(): void {
-                this.activeModal.dismiss();
-              }
-            
-              addPhoto() {
-                const request = new FormData();
-            
-                if (this.attachedFile) {
-                  this.fileType = '7';
-                  request.append('Data.FileDto.File', this.attachedFile);
-                  request.append('Data.FileDto.Type', this.fileType);
-                }
-                request.append('Data.ContractorId', this.contractorId);
-                request.append('Data.MediaFileId', this.uploadForm.value.mediaFileId);
-            
-                this.contractorService.editAvatar(request).subscribe(()=>
-                  this.notificationService.success('Success', 'Picture added!', NotificationUtil.getDefaultMidConfig()));
-                  this.activeModal.close(this.uploadForm.value);
-              }
-            
-              checkFile(event) {
-                if (event != null) this.attachedFile = event;
-                else this.fileId = null;
-              }
-
+      forkJoin([
+        this.translate.get('modal.success'),
+        this.translate.get('notification.body.success.picture')
+      ]).subscribe(
+        ([ success, picture ]) => {
+          this.contractorService.editAvatar(request).subscribe(()=>
+          this.notificationService.success(success, picture, NotificationUtil.getDefaultMidConfig()));
+          this.activeModal.close(this.uploadForm.value);
+        });
+      }
+      
+      checkFile(event) {
+        if (event != null) 
+        this.attachedFile = event;
+        else 
+        this.fileId = null;
+      }
 }
