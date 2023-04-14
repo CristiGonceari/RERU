45cb92c448a7821c20c17cb2e438204ceb4711e5
Diana Ribac <diana.ribac@codwer.com>
2023-04-14 10:37:11 +0300

    fix document templates order by descending

--- src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/DocumentsTemplates/GetAndFilterDocumentTemplates.cs
+++ src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/DocumentsTemplates/GetAndFilterDocumentTemplates.cs
@@ -11,7 +11,9 @@ namespace CODWER.RERU.Evaluation.Application.DocumentsTemplates
     {
         public static IQueryable<DocumentTemplate> Filter(AppDbContext appDbContext, string name, FileTypeEnum fileType)
         {
-            var documentTemplates = appDbContext.DocumentTemplates.AsQueryable();
+            var documentTemplates = appDbContext.DocumentTemplates
+                .OrderByDescending(x => x.Id)
+                .AsQueryable();
 
             if (!string.IsNullOrWhiteSpace(name))
             {
