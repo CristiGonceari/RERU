7e5fe1ef760ddb9abdd6a2c2756010c3e7fdd81f
Evghenia Ivanova <evghenia.ivanova@codwer.com>
2023-05-09 12:23:01 +0300

    fix DocumentTemplateKeys

---
+++ src/Microservices/CODWER.RERU.Identity/CODWER.RERU.IdentityMigrator.Console/Migrations/AppDb/20230509085747_fixDocumentTemplateKeys.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CODWER.RERU.IdentityMigrator.Console.Migrations.AppDb
+{
+    public partial class fixDocumentTemplateKeys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE \"DocumentTemplateKeys\" SET \"FileType\" = 3 WHERE \"DocumentTemplateKeys\".\"FileType\" = 2");
+            migrationBuilder.Sql("UPDATE \"DocumentTemplateKeys\" SET \"FileType\" = 2 WHERE \"DocumentTemplateKeys\".\"FileType\" = 1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE \"DocumentTemplateKeys\" SET \"FileType\" = 1 WHERE \"DocumentTemplateKeys\".\"FileType\" = 2");
+            migrationBuilder.Sql("UPDATE \"DocumentTemplateKeys\" SET \"FileType\" = 2 WHERE \"DocumentTemplateKeys\".\"FileType\" = 3");
+        }
+    }
+}
