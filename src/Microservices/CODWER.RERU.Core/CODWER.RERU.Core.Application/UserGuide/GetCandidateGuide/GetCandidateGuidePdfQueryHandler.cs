d52c2dd9b87761a15e618bfb8d2b052d02a5f6bb
Evghenia Ivanova <evghenia.ivanova@codwer.com>
2023-05-16 18:00:25 +0300

    Addition of the candidate's guide with the possibility of download on the registration page

---
+++ src/Microservices/CODWER.RERU.Core/CODWER.RERU.Core.Application/UserGuide/GetCandidateGuide/GetCandidateGuidePdfQueryHandler.cs
@@ -0,0 +1,19 @@
+using CVU.ERP.Common.DataTransferObjects.Files;
+using MediatR;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CODWER.RERU.Core.Application.UserGuide.GetCandidateGuide
+{
+    public class GetCandidateGuidePdfQueryHandler : IRequestHandler<GetCandidateGuidePdfQuery, FileDataDto>
+    {
+        public async Task<FileDataDto> Handle(GetCandidateGuidePdfQuery request, CancellationToken cancellationToken)
+        {
+            var path = new FileInfo("UserGuide/Ghidul candidatului.pdf").FullName;
+            var bytes = await File.ReadAllBytesAsync(path);
+
+            return FileDataDto.GetPdf("Ghidul candidatului.pdf", bytes);
+        }
+    }
+}
