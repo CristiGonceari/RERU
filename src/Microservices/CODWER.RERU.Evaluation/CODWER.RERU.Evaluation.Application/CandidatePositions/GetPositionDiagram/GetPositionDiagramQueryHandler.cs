6668f0e374768871895d08f9a0bdd2bad26f0c3c
Octavian Magdei <octavian.magdei@codwer.com>
2023-06-23 16:34:39 +0300

    update order by update time

--- src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/CandidatePositions/GetPositionDiagram/GetPositionDiagramQueryHandler.cs
+++ src/Microservices/CODWER.RERU.Evaluation/CODWER.RERU.Evaluation.Application/CandidatePositions/GetPositionDiagram/GetPositionDiagramQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -82,6 +83,9 @@ namespace CODWER.RERU.Evaluation.Application.CandidatePositions.GetPositionDiagr
                 .Where(x => x.EventUser.EventId == eventId && x.CandidatePositionId == positionId)
                 .Select(x => x.EventUser.UserProfile)
                 .OrderByFullName()
+                .OrderByDescending(x => x.SolicitedVacantPositions
+                    .Any() ? x.SolicitedVacantPositions
+                    .Min(y => y.UpdateDate) : DateTime.MaxValue)
                 .ToList();
 
             var mappedUsers = _mapper.Map<List<UserDiagramDto>>(users);
@@ -174,13 +178,6 @@ namespace CODWER.RERU.Evaluation.Application.CandidatePositions.GetPositionDiagr
 
                 user.TestsByTestTemplate = user.TestsByTestTemplate.OrderBy(x => x.EventId).ThenBy(x => x.TestTemplateId).ToList();
             }
-
-            eventDiagram.UsersDiagram = eventDiagram.UsersDiagram
-                    .OrderByDescending(user => user.TestsByTestTemplate
-                        .SelectMany(testTemplate => testTemplate.Tests)
-                        .Max(test => test.PassDate))
-                    .ToList();
-
         }
     }
 }
